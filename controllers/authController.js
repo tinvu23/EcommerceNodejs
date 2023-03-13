6b904a5bc397a51dabbf20a8931ab7aeecfaf194
huudang1631 <huudangntp@gmail.com>
2023-03-13 23:04:26 +0700

    Dang commit 2: register API

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,44 +1,51 @@
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import userModel from "../models/userModel.js";
-
+import JWT from "jsonwebtoken";
 
 export const registerController = async (req , res) => {
     try {
-        const {name,email,password,phone,address} = req.body
+        const {name,email,password,phone,address} = req.body;
         //validations
         if(!name){
-            return res.send({error:'Name is reqired'})
+            return res.send({error:"Name is reqired"});
         }
         if(!email){
-            return res.send({error:'Email is reqired'})
+            return res.send({error:"Email is reqired"});
         }
         if(!password){
-            return res.send({error:'Password is reqired'})
+            return res.send({error:"Password is reqired"});
         }
         if(!phone){
-            return res.send({error:'Phone no is reqired'})
+            return res.send({error:"Phone no is reqired"});
         }
         if(!address){
-            return res.send({error:'Address is reqired'})
+            return res.send({error:"Address is reqired"});
         }
         
-        //check user
+        //chekck user
         const exisitingUser = await userModel.findOne({email})
+
         //exisiting user
         if(exisitingUser){
             return res.status(200).send({
                 success:true,
-                message:'Already Register please login',
+                message:"Already Register please login",
             })
         }
         //register user
-        const hassedPassword = await hashPassword(password)
+        const hassedPassword = await hashPassword(password);
         //save
-        const user = new userModel({name, email, phone, address, password:hassedPassword}).save()
+        const user = await new userModel({
+            name, 
+            email, 
+            phone, 
+            address, 
+            password:hassedPassword,
+        }).save()
 
         res.status(201).send({
             success:true,
-            message:'User Register Successfully',
+            message:"User Register Successfully",
             user,
         })
 
@@ -46,11 +53,66 @@ export const registerController = async (req , res) => {
         console.log(error)
         res.status(500).send({
             success:false,
-            message:'Error in Registeration',
+            message:"Error in Registeration",
             error
         })
     }
 };
 
+
+
+//POST LOGIN
+ export const loginController = async (req , res) => {
+    try {
+        const {email, password} = req.body;
+        //validation
+        if(!email || !password){
+            return res.status(404).send({
+                success:false,
+                message:"Invalid email or password"
+            })
+        }
+        //check user
+        const user = await userModel.findOne({email})
+        if(!email){
+            return res.status(404).send({
+                success:false,
+                message:"Email is not registerd"
+            })
+        }
+        
+        const match = await comparePassword(password,user.password)
+        if(!match){
+            return res.status(200).send({
+                success:false,
+                message:"Invalid Password"
+            })
+        }
+
+        //token
+        const token = await JWT.sign({_id:user._id}, process.env.JWT_SECRET,
+            {expiresIn: "7d",
+        });
+
+        res.status(200).send({
+            success:true,
+            message:"Login Successfully",
+            user:{
+               name:user.name,
+               email:user.email,
+               phone:user.phone,
+               address:user.address,
+            },
+            token
+        });
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:"Error in Login",
+            error
+        })
+    }
+ }
 
 
